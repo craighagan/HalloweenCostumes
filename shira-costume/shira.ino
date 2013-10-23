220a8938481403593e396454ca8e836b95fe52a3
hagan <hagan@tv.(none)>
2013-10-23 09:06:41 -0400

    update shira's costume to reflect dress change blink rate to more quickly detect button on

--- shira-costume/shira.ino
+++ shira-costume/shira.ino
@@ -2,23 +2,26 @@
 #include "LED.h"
 #include "BUTTON.h"
 
-#define NR_LEDS 5
+#define NR_LEDS 7
 
 LED leds[] = {
   LED(13),
+  LED(A2),
+  LED(10),
   LED(5),
   LED(A5),
-  LED(A4),
-  LED(6)
+  LED(6),
+  LED(9)
 };
 
 int i;
 
-BUTTON button(11);
+BUTTON button(A3);
+//BUTTON button(11);
 
 void setup() {
-   //Serial.begin(9600);
-  
+   Serial.begin(9600);
+   Serial.print("hello world\n");
    for(i=0;i<NR_LEDS;i++) {
       leds[i].off(); 
    }
@@ -30,18 +33,24 @@ void loop() {
   is_pressed = button.pressed();
   
   if (is_pressed) {
-    //Serial.print("button pressed\n");
-    for(i=0;i<NR_LEDS&&is_pressed;i++) {
-       is_pressed = button.pressed();
+    Serial.print("button pressed\n");
+    for(i=0;i<NR_LEDS&&is_pressed;i++) {       
        leds[i].blink(40);
-       delay(40);
+       is_pressed = button.pressed();
     }
   } else {
-
+     Serial.print("button not pressed\n");
      for(i=0;i<NR_LEDS&&!is_pressed;i++) {
-       is_pressed = button.pressed();
        leds[i].blink(200);
-       delay(750);
-     }
+       delay(100);
+       is_pressed = button.pressed();
+       delay(100);
+       is_pressed = button.pressed();
+       delay(100);
+       is_pressed = button.pressed();
+       delay(100);
+       is_pressed = button.pressed();       
+     }     
   }
+  
 }
