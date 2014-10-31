d076866944114d5d51a1d9795e78dfc58066f8b7
hagan <hagan@cih.com>
2014-10-30 22:02:53 -0400

    Have the costume play a sound every two minutes

--- ben-costume/test-button.py
+++ ben-costume/test-button.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import glob
-from time import sleep
+from time import sleep, time
 import os
 import RPi.GPIO as GPIO
 from itertools import cycle
+
+last_played_time = 0
+sound_interval = 2*60
 
 GPIO.setmode(GPIO.BCM)
 
@@ -25,19 +28,29 @@ class Button(object):
 
 		print "pin %d list %s" % (self._pin, repr(self._mp3s))
 
+	def play_sound(self):
+		global last_played_time
+
+		try:
+			mp3 = self._mp3_iter.next()
+			print("would play %s" % (mp3))
+			os.system("mpg321 -q %s" % (mp3))
+			while GPIO.input(self._pin) == False:
+				sleep(0.1)
+
+			last_played_time = time()
+			print "set last_played_time to %s" % last_played_time
+
+
+		except StopIteration:
+			pass
+
 	def checkpin(self):
 		res = GPIO.input(self._pin) 
+		
 		if ( res  == False):
-			try:
-				mp3 = self._mp3_iter.next()
-				print("would play %s" % (mp3))
-				os.system("mpg321 -q %s" % (mp3))
-				
-				while GPIO.input(self._pin) == False:
-					sleep(0.1)
+			self.play_sound()
 
-			except StopIteration:
-				pass
 
 
 button_array = []
@@ -50,9 +63,19 @@ button_array.append(Button(18,"/home/pi/ben-costume/buttons/bell"))
 
 #ding on startup`
 os.system("mpg321 -q /home/pi/ben-costume/buttons/bell/bell.mp3")
+last_played_time = time()
+last_played_button = 0
 
 while True:
 	for button in button_array:
 		button.checkpin()
-	sleep(0.1);
 
+	if (last_played_time + sound_interval) < time():
+		print "last played is %s curtime is %s" % (last_played_time+sound_interval, time())
+		button_array[last_played_button].play_sound()
+		last_played_button += 1
+
+		if last_played_button >= len(button_array):
+			last_played_button = 0
+
+	sleep(0.1)
